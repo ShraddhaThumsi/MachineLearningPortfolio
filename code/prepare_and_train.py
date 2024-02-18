a34ea6890c68331a90440aa55d76961b9f08e94b
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2024-02-18 17:32:20 +0530

    The theoretical material covers lot of ground on the stats model and how to use it to train a Linear Regression. This code trains a Linear regression model using the sklearn package. The next commit will be to see how other features are related to score and how our model performs on it.

--- code/prepare_and_train.py
+++ code/prepare_and_train.py
@@ -1,3 +1,18 @@
 import read_examine_soccer_data as data_source
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
 soccer_df=data_source.df
 print(soccer_df.shape)
+#for the purpose of this mini-project, let us consider the cost of the player as the predictor variable and score obtained as target variable
+#In the "read_examine_soccer_data" file we saw a Pearson coeeficient of +0.96, and hence this choice of predictor variable.
+
+x=np.array(soccer_df['Cost']).reshape(-1,1)
+y=np.array(soccer_df['Score']).reshape(-1,1)
+#now we will split our available data into train test sets
+X_train, X_test, y_train, y_test = train_test_split(x,y,train_size=0.75,test_size=0.25,random_state=50)
+
+#the theoretical material gotten from ProjectPro implements the regression using Statsmodels package. Here I am doing it using sklearn
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+print(lr.score(X_test,y_test)) #the statsmodel version has a score of 0.94, and we are at 0.93.
--- code/read_examine_soccer_data.py
+++ code/read_examine_soccer_data.py
@@ -1,8 +1,38 @@
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 path_to_file = '../data/linear_regression_part1_soccerdata.csv'
 df = pd.read_csv(path_to_file)
 
 
-# plt.scatter(df['Cost'],df['Score'])
-# plt.show()
+
+#EDA on our soccer dataset
+#print(df['Club'].value_counts())
+
+#Statistical features of each column of dataset, including quartiles and standard deviation
+#print(df.describe())
+
+#uncovering the datatype of each column
+#print(df.info())
+
+#examining the pearson correlation matrix
+#print(df.corr())
+
+#cost to hire the player in a club and the score obtained by them in a match is positively correlated with a Pearson coefficient of +0.96
+plt.scatter(df['Cost'],df['Score'])
+plt.title('Cost to hire one player v/s score obtained by them')
+#plt.show()
+
+#the distance run by a player in a game and the score obtained by them is negatively correlated with a Pearson coefficient of -0.49.
+#This means that longer the distance that a player runs befor ehe makes a goal, the lower his game score will be.
+
+
+plt.scatter(df['DistanceCovered(InKms)'],df['Score'])
+plt.title('Distance covered v/s score obtained by one player')
+#plt.show()
+
+
+# Let's examine this with the time taken by the player to make a goal -
+plt.scatter(df['MinutestoGoalRatio'],df['Score'])
+plt.title('Time taken to make a goal v/s score obtained by one player')
+#plt.show()
+#as expected, the longer the player takes before making his goal, the lower his game score will be
