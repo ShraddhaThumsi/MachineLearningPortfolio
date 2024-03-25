ed44005145861f0d16a100cc7e8548526366e28b
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2024-03-25 11:56:28 +0530

    adding image segmentation dataset, imports and custom loss functions

---
+++ image_recognition/semantic_segmentation/code/task.py
@@ -0,0 +1,29 @@
+#importing necessary libraries
+import pandas as pd
+import numpy as np
+import zipfile
+import os
+import glob
+import random
+import sys
+
+import skimage.io as io
+import skimage.transform as transform
+from skimage.morphology import label
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, LearningRateScheduler
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split as train_test_split
+from tensorflow.keras.losses import binary_crossentropy as binary_cross_entropy
+
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+IMG_CHANNELS = 3
+
+LEARNING_RATE = 0.001
+NUM_EPOCHS=25
+
