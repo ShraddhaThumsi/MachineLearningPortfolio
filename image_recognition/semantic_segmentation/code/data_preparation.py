b9b7cb96cd69d0b5ff3cb5800b34ad5bf5bea60a
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2024-04-02 21:23:46 +0530

    i am trying parsing using medpy package.. it is working but now i need to figure out how to meld with the other datasets. That's for tomorrow.

--- image_recognition/semantic_segmentation/code/data_preparation.py
+++ image_recognition/semantic_segmentation/code/data_preparation.py
@@ -7,7 +7,7 @@ import imageio
 import skimage
 import numpy as np
 import matplotlib.pyplot as plt
-
+from medpy.io import load
 extn = 'png'
 width = 256
 height = 256
@@ -22,43 +22,42 @@ brca_image_path = '../data/breast_cancer_histopathology/Images/'
 brca_label_path = '../data/breast_cancer_histopathology/Masks/'
 
 hippo_rootpath = '../data/hippocampus/'
-hippo_image_path = '../data/hippocampus/imagesTr/'
-hippo_label_path = '../data/hippocampus/labelsTr/'
+hippo_image_path = '../data/hippocampus/imagesTr_original_data/'
+hippo_label_path = '../data/hippocampus/labelsTr_original_data/'
+
+def process_brca(root_path):
+    def is_tif_file(filename):
+        filename = filename.lower()
+        return filename.endswith('.tif') and '.xml' not in filename
+
+    img_paths = sorted([f'{root_path}{i}' for i in os.listdir(root_path)])
+    print('image paths are loaded for brca')
+    img_paths = list(filter(is_tif_file, img_paths))
+
+    return img_paths
+def process_hippo(root_path):
+    hippo_image_paths = sorted([f'{root_path}{i}' for i in os.listdir(root_path)])
+
+    def convert_file_to_png(filename):
+        image_data,image_header = load(filename)
+
+        return image_data
+
+    img_paths = list(map(convert_file_to_png, hippo_image_paths))
+    print(img_paths[0])
+    return img_paths
+
 def get_images(root_path,output_size,is_label=False,is_brca=False, is_hippocampus=False):
     img_paths = sorted([f'{root_path}{i}' for i in os.listdir(root_path)])
-
-
     if is_brca:
 
         #parses the breast cancer dataset. The folder contains only tif and .xml files so we have to read it along with cleaning up file names
-        def is_tif_file(filename):
-            filename=filename.lower()
-            return filename.endswith('.tif') and '.xml' not in filename
-
-        img_paths = sorted([f'{root_path}{i}' for i in os.listdir(root_path)])
-        print('image paths are loaded for brca')
-        img_paths = list(filter(is_tif_file, img_paths))
+        img_paths = process_brca(root_path)
 
     if is_hippocampus:
-
-        hippo_image_paths = sorted([f'{root_path}{i}' for i in os.listdir(root_path)])
-        def convert_file_to_png(filename):
-            if '.png' not in filename:
-                name_only = filename[:-4]
-                png_path = f'{name_only}.png'
-                nii_image = nibabel.load(filename)
-
-                nii_data = nii_image.get_data()
-                nii_data = np.clip(nii_data, 0, 255).astype(np.uint8)
-
-
-                img = Image.fromarray(nii_data,mode='RGB')
-                img.save(png_path)
-                return png_path
-            else:
-                return filename
-        img_paths = list(map(convert_file_to_png,hippo_image_paths))
-
+        img_paths = process_hippo(root_path)
+        print('inside hippocampus transformation condition')
+        print(len(img_paths[0]))
 
 
 
@@ -81,23 +80,28 @@ def get_images(root_path,output_size,is_label=False,is_brca=False, is_hippocampu
 #importing breast cancer images
 X_brca_data = get_images(brca_image_path,output_size=(height,width),is_brca=True)
 Y_brca_data = get_images(brca_label_path,output_size=(height,width),is_brca=True,is_label=True)
-
+print('shape of brca data')
+print(X_brca_data.shape)
+print(Y_brca_data.shape)
 #importing colorectal images
 X_data = get_images(image_path,(height,width))
 Y_data = get_images(label_path,(height,width),is_label=True)
-
+print('shape of colorectal data')
+print(X_data.shape)
+print(Y_data.shape)
 #importing hippocampal images
 X_hippo_data = get_images(hippo_image_path,(height,width),is_hippocampus=True)
 Y_hippo_data = get_images(hippo_label_path,(height,width),is_hippocampus=True,is_label=True)
-print('shape of x hippocampus data')
+print('shape of hippocampus data')
 print(X_hippo_data.shape)
-print('shape of y hippocampus data')
 print(Y_hippo_data.shape)
 
 #appending for larger dataset
-X_data = np.concatenate((X_data,X_brca_data),axis=0)
-Y_data = np.concatenate((Y_data,Y_brca_data),axis=0)
-
+X_data = np.concatenate((X_data,X_brca_data,X_hippo_data),axis=0)
+Y_data = np.concatenate((Y_data,Y_brca_data,Y_hippo_data),axis=0)
+print('shape of full data after concatenating various cancers data')
+print(X_data.shape)
+print(Y_data.shape)
 
 f, axarr = plt.subplots(2,4)
 f.set_size_inches(20,10)
