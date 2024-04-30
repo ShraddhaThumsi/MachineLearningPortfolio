a4af27ddedee5dba4e4d8ca0f6df16593e3aef83
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2024-04-30 17:43:20 +0530

    Writing covariance matrix to log file

--- time series/sensor_reading_prediction/Runner.py
+++ time series/sensor_reading_prediction/Runner.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import sys
+import numpy as np
+from datetime import datetime
 import utils.PreProcessTimeSeriesData as PreProcess
 path_to_input_file = './data/Data-Chillers.csv'
 sensor_data_df = pd.read_csv(path_to_input_file)
 processed_df = PreProcess.clean_time_series(sensor_data_df)
-print(processed_df.shape)
-print("first five entries after cleaning")
-print(processed_df.head())
-print("last five entries after cleaning")
-print(processed_df.tail())
+df = processed_df.set_index('time')#the primary index in the original df is a row number- an integer value. When we make the time column as the index, the only other column in the dataset is the sensor value. The covariance matrix is defined for only one feature at a time
+
+with open('./output/'+'covariance_log.txt','a') as f:
+    sys.stdout=f
+    print('Run time: ', datetime.now())
+    print('*********Covariance matrix is as follows: *******')
+    print(np.cov(df,rowvar=False))
+sys.stdout =  sys.__stdout__
--- time series/sensor_reading_prediction/utils/PreProcessTimeSeriesData.py
+++ time series/sensor_reading_prediction/utils/PreProcessTimeSeriesData.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def clean_time_series(time_series_df,is_univariate=True):
     time_series_df.time=pd.to_datetime(time_series_df.time, format='%d-%m-%Y %H:%M') #the format here represents the format in which the data appears, not the format in which we want to store the cleaned up date column
-    time_series_df=time_series_df.sort_values(by=['time'])
+    time_series_df=time_series_df.sort_values('time')
     if is_univariate:
         del time_series_df['Error_Present']
         del time_series_df['Sensor_2']
