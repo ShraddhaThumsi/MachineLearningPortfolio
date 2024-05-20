2a3faa7b3f62c17be8dc07b37486d7bb101613cf
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2024-05-20 14:33:31 +0530

    added code for feature extraction and concatenation

--- NLP/detecting gene changes from medical literature/Runner.py
+++ NLP/detecting gene changes from medical literature/Runner.py
@@ -2,6 +2,7 @@ import utils.ReadLiteratureData as read
 import utils.EDA as EDA
 import utils.CleanPreprocessData as Preprocess
 from sklearn.model_selection import train_test_split
+import utils.Models as Models
 train_data,test_data = read.read_and_clean_df()
 print(train_data.shape)
 EDA.analyze_gene_data(train_data)
@@ -10,6 +11,20 @@ print(train_data.shape)
 print(train_data['Text'])
 y_true = train_data['Class'].astype(int)
 del train_data['Class']
+#data and code reference from ProjectPro
 #we want the same distribution on the train,validation and test sets, so we will split accordingly
 X_train, X_rem, y_train, y_rem = train_test_split(train_data, y_true, stratify=y_true, test_size=0.2)
 X_val, X_test, y_val, y_test = train_test_split(X_rem, y_rem, stratify=y_rem, test_size=0.5)
+X_train_gene_onehot,X_val_gene_onehot,X_test_gene_onehot=Models.transform_features_into_numer(X_train['Gene'],X_val['Gene'],X_test['Gene'])
+X_train_variation_onehot,X_val_variation_onehot,X_test_variation_onehot=Models.transform_features_into_numer(X_train['Variation'],X_val['Variation'],X_test['Variation'])
+X_train_text_feature,X_val_text_feature,X_test_text_feature=Models.extract_text_features(X_train['Text'],X_val['Text'],X_test['Text'],min_df_value=400)
+print('showing shape of text feature of training data, validation data and testing data in that order')
+print(X_train_text_feature.shape)
+print(X_val_text_feature.shape)
+print(X_test_text_feature.shape)
+X_train_new=Models.concatenate_features(X_train_gene_onehot,X_train_variation_onehot,X_train_text_feature,print_this='train')
+X_val_new=Models.concatenate_features(X_val_gene_onehot, X_val_variation_onehot,X_val_text_feature,print_this='val')
+X_test_new=Models.concatenate_features(X_test_gene_onehot, X_test_variation_onehot, X_test_text_feature,print_this='test')
+print('shape of training data after feature concatenation',X_train_new.shape)
+print('shape of validation data after feature concatenation',X_val_new.shape)
+print('shape of testing data after feature concatenation',X_test_new.shape)
--- NLP/detecting gene changes from medical literature/utils/Models.py
+++ NLP/detecting gene changes from medical literature/utils/Models.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import normalize
 from sklearn import metrics
+import pandas as pd
 
 def evaluate_model(model,test_features,y_truth):
     prediction=model.predict(test_features)
@@ -10,3 +11,48 @@ def evaluate_model(model,test_features,y_truth):
     predict_probabilities= model.predict_proba(test_features)
     logloss_metric=log_loss(y_truth,predict_probabilities)
     return logloss_metric
+
+def transform_features_into_numer(X_train,X_val,X_test):
+    train_feature_onehotCoding = pd.get_dummies(X_train, drop_first=True)
+
+    val_feature_onehotCoding = pd.get_dummies(X_val, drop_first=True)
+    # we use reindex to handle the unknown categories which didnt appear in the training data
+    val_feature_onehotCoding = val_feature_onehotCoding.reindex(columns=train_feature_onehotCoding.columns,
+                                                                fill_value=0)
+
+    test_feature_onehotCoding = pd.get_dummies(X_test, drop_first=True)
+    test_feature_onehotCoding = test_feature_onehotCoding.reindex(columns=train_feature_onehotCoding.columns,
+                                                                  fill_value=0)
+
+    return train_feature_onehotCoding, val_feature_onehotCoding, test_feature_onehotCoding
+
+
+def extract_text_features(X_train,X_val,X_test,min_df_value):
+    text_vectorizer = TfidfVectorizer(min_df=min_df_value, stop_words="english")
+
+    train_text_feature_onehotCoding = text_vectorizer.fit_transform(X_train)
+    val_text_feature_onehotCoding = text_vectorizer.transform(X_val)
+    test_text_feature_onehotCoding = text_vectorizer.transform(X_test)
+
+    train_text_feature_onehotCoding = normalize(train_text_feature_onehotCoding, axis=0)
+    val_text_feature_onehotCoding = normalize(val_text_feature_onehotCoding, axis=0)
+    test_text_feature_onehotCoding = normalize(test_text_feature_onehotCoding, axis=0)
+
+    return train_text_feature_onehotCoding, val_text_feature_onehotCoding, test_text_feature_onehotCoding
+def concatenate_features(gene_feature,variation_feature,text_feature,print_this='what is your name?'):
+    print()
+    print('***********************')
+    print('inside concatenate function, checking shape of each incoming feature')
+    print(print_this.upper())
+    print('gene feature shape', gene_feature.shape)
+    print('variation feature shape', variation_feature.shape)
+    print('text feature shape', text_feature.shape)
+    print('***********************')
+    print()
+
+    '''Concatenate all extracted features together'''
+    gene_variation_feature=pd.concat([variation_feature,gene_feature],axis=1)
+    text_feature= pd.DataFrame(text_feature.toarray())
+    gene_variation_feature.reset_index(drop=True, inplace=True)
+    gene_variation_text_feature=pd.concat([text_feature,gene_variation_feature],axis=1)
+    return gene_variation_text_feature
