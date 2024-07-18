ee05ed736eb6871b3b111d62b328f1ec15de8130
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2024-07-18 13:06:47 +0530

    I am now concluding this starter project of a basic comparison of ARIMA, ARCH and GARCH models. For ARCH and GARCH, I am using a geometric Brownian motion to calculate the next stock price.

--- time series/ARCH & GARCH/code/MarketSimulatorUtils.py
+++ time series/ARCH & GARCH/code/MarketSimulatorUtils.py
@@ -1,10 +1,10 @@
 import numpy as np
 from arch import arch_model
 from statsmodels.tsa.arima.model import ARIMA
-def calculate_future_prices(current_price,future_volatilities):
+def calculate_future_prices(current_price,future_volatilities,mu):
     future_prices = []
     for next_day_vol in future_volatilities:
-        future_price = current_price*np.exp(np.sqrt(next_day_vol))
+        future_price = current_price*np.exp((mu-0.5*next_day_vol)+np.sqrt(next_day_vol)*np.random.randn())
         future_prices.append(future_price)
     return future_prices
 
@@ -14,8 +14,12 @@ def get_arima_forecast(prices,horizons):
     forecast=model_fit.predict(steps=horizons)
     return forecast
 
-def garch_forecast(returns,horizons):
-    model = arch_model(returns,vol='GARCH',p=1,q=1)
+def garch_forecast(returns,horizons,is_garch=True,lags=1):
+    if is_garch:
+
+        model = arch_model(returns,vol='GARCH',p=lags,q=1)
+    else:
+        model = arch_model(returns, vol='ARCH',p=lags)
     model_fit=model.fit()
     forecast = model_fit.forecast(horizon=horizons)
     return forecast.variance.values[-1]
--- time series/ARCH & GARCH/code/Runner.py
+++ time series/ARCH & GARCH/code/Runner.py
@@ -4,45 +4,65 @@ import MarketSimulatorUtils as MktSimUtils
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date
+from matplotlib.ticker import FuncFormatter
 import datetime
 source_of_csv = '/Users/shraddha/datascience/python_code/MachineLearningPortfolio/time series/ARCH & GARCH/data/NIFTY 50'
 path_to_output = '/Users/shraddha/datascience/python_code/MachineLearningPortfolio/time series/ARCH & GARCH/output'
 nifty_timeseries_df_per_company = NiftyReader.make_dataset(source_of_csv,path_to_output)
-print(type(nifty_timeseries_df_per_company))
-horizons=20
-list_of_companies_to_use = ['ADANI_ENTERPRISES']
-#list_of_companies_to_use = list(nifty_timeseries_df_per_company.keys())
-for company in list_of_companies_to_use:
-    print(f'printing {horizons} horizons for company {company}')
-    train,test=Predictor.split_train_test(nifty_timeseries_df_per_company[company])
-    Predictor.calculate_pct_change_in_volatility(train,path_to_output)
-    trained_arch1_model = Predictor.make_arch_model(train,path_to_output)
-    trained_garch_model = Predictor.make_garch_model(train,path_to_output)
-    arch_prediction = trained_arch1_model.forecast(horizon=horizons)
-    print(arch_prediction.residual_variance)
-    garch_prediction = trained_garch_model.forecast(horizon=horizons)
-    print(garch_prediction.residual_variance)
+
+horizons=30
+#list_of_companies_to_use = ['ADANI_PORTS','ADANI_ENTERPRISES','APOLLO HOSPITALS']
+#list_of_companies_to_use = ['ADANI_ENTERPRISES']
+list_of_companies_to_use = list(nifty_timeseries_df_per_company.keys())
+#column_to_use = 'VWAP'
+#column_to_use = 'Adj Close'
+column_to_use='Close'
+mu = 0.05 # annual risk free return rate in India
+
+
 
 
 #the ARCH and GARCH models give us the volatility in the price of the stock. That's interesting, but we can beef it up by predicting the stock price itself, and also simulating market conditions to get a full picture. Upto now we performed a basic GARCH model. Now we will direct the rest of the code into a stock price predictor set-up. We will also attemot to deploy it.
 for company in list_of_companies_to_use:
     company_wise_data = nifty_timeseries_df_per_company[company]
-    print(len(list(company_wise_data.index)))
-    arima_forecast_prices = MktSimUtils.get_arima_forecast(company_wise_data['VWAP'],horizons=horizons)
-    print(f'got the arima forecast prices until {horizons} horizons for company {company}')
-    returns = np.diff(company_wise_data['VWAP']) / company_wise_data['VWAP'][:-1]
-    volatilities = MktSimUtils.garch_forecast(returns,horizons)
-    current_price = company_wise_data['VWAP'][-1]
+    true_prices = company_wise_data[column_to_use][-horizons:]
+    print(f'for company {company} the following are the true prices of the stock for past {horizons} days')
+    print(true_prices)
+    arima_forecast_prices = MktSimUtils.get_arima_forecast(company_wise_data[column_to_use],horizons=horizons)
+
+    returns = (np.diff(company_wise_data[column_to_use]) / company_wise_data[column_to_use][:-1])
+    print(returns)
+    garch_volatilities = MktSimUtils.garch_forecast(returns, horizons)
+    print(f'garch volatilities are : {garch_volatilities}')
+
+    arch_volatilities=MktSimUtils.garch_forecast(returns, horizons,is_garch=False)
+    print(f'arch volatilities are : {arch_volatilities}')
+
+    current_price = company_wise_data[column_to_use][-1]
     last_available_dateas_str = list(company_wise_data.index)[-1]
     last_available_date = date(int(last_available_dateas_str[-4:]),int(last_available_dateas_str[3:5]),int(last_available_dateas_str[0:2]))
-    print(last_available_date)
     date_list = [last_available_date - datetime.timedelta(days=x) for x in range(horizons)]
-    print(len(date_list))
-    date_list=[str(d) for d in date_list]
+
+    date_list=sorted([str(d) for d in date_list])
     #there is something worth pointing here. The results from a GARCH model are tethered to a date with the most complete information. We have to recallibrate the garch model each day when new stovk prices become official. But since that is out of scope pf this project, I will assume the same volatility across and use that to predict for any day in the future.
-    future_prices = MktSimUtils.calculate_future_prices(current_price,volatilities)
-    plt.plot(date_list, future_prices, marker='x', linestyle='-', color='blue',
+    garch_future_prices = MktSimUtils.calculate_future_prices(current_price, garch_volatilities,mu)
+    print(f'future prices from garch vols: {garch_future_prices}')
+    arch_future_prices = MktSimUtils.calculate_future_prices(current_price, arch_volatilities,mu)
+    print(f'future prices from arch vols: {arch_future_prices}')
+    plt.figure(figsize=(20, 8))
+    plt.plot(date_list, garch_future_prices, marker='x', linestyle='-', color='red',
              label='Future Prices (GARCH Forecasted Volatility)')
-    plt.savefig(path_to_output + '/logs/' + f'garch_predictions_of_volatility_{company}' + '.png')
+    plt.plot(date_list, arima_forecast_prices[-horizons:], marker='o', linestyle='-', color='green',
+             label='Future Prices (ARIMA Forecasted)')
+    plt.plot(date_list, arch_future_prices, marker='v', linestyle='-', color='cyan',
+             label='Future Prices (ARCH Forecasted)')
+    plt.plot(date_list, true_prices, marker='*', linestyle='-', color='black',
+             label=f'true prices of {column_to_use} for {horizons} days')
+    plt.xticks(rotation=60)
+    plt.legend()
+
+    plt.title(f'{column_to_use} forecasts for {company} by ARIMA, ARCH & GARCH on {horizons} horizons')
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda x, _: '{:.0f}'.format(x)))
+    plt.savefig(path_to_output + '/logs/' + f'stock_price_predictions_{horizons}_horizons_{company}' + '.png')
     plt.close()
     plt.show()
